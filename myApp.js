ea1d73d144b322f2cf7ff0ed36254321b1168346
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-01-13 13:22:46 -0600

    changes

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 const absolutePath = __dirname + "/views/index.html";
@@ -6,9 +7,18 @@ const middleWarePath = __dirname + "/public";
 // app.get("/", function (req, res) {
 //   res.send("Hello Express");
 // });
+app.get('/json', function(req, res) {
+  const mySecret = process.env['MESSAGE_STYLE'];
+  if (mySecret === "uppercase") {
+    res.json({ "message": "Hello json".toUpperCase() })
+  } else {
+    res.json({ "message": "Hello json" })
+  }
+
+});
+
 app.use("/public", express.static(middleWarePath));
 app.get("/", (req, res) => res.sendFile(absolutePath));
-app.get("/json", (req, res) => res.json({"message":"Hello json"}));
 
 console.log("Hello World");
 module.exports = app;
