9de6383a9db412d5956854d6f9be31ca4f690955
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-01-13 10:58:37 -0600

    api and json testing

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
 let express = require("express");
 let app = express();
-const absolutePath = __dirname + '/views/index.html'
+const absolutePath = __dirname + "/views/index.html";
+const middleWarePath = __dirname + "/public";
 
 // app.get("/", function (req, res) {
 //   res.send("Hello Express");
 // });
-
-app.get("/", (req,res) => res.sendFile(absolutePath));
+app.use("/public", express.static(middleWarePath));
+app.get("/", (req, res) => res.sendFile(absolutePath));
+app.get("/json", (req, res) => res.json({"message":"Hello json"}));
 
 console.log("Hello World");
 module.exports = app;
