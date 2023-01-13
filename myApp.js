7bd714b81877f30d35aeb59860311e3cd1f68fd5
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-01-13 09:22:40 -0600

    changes

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require("express");
 let app = express();
+const absolutePath = __dirname + '/views/index.html'
 
-app.get("/", function (req, res) {
-  res.send("Hello Express");
-});
+// app.get("/", function (req, res) {
+//   res.send("Hello Express");
+// });
+
+app.get("/", (req,res) => res.sendFile(absolutePath));
 
 console.log("Hello World");
 module.exports = app;
