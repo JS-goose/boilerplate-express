33d66844fd76e499b782f039a61585cc5b609029
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-01-14 10:20:27 -0600

    notes

--- myApp.js
+++ myApp.js
@@ -4,10 +4,12 @@ let app = express();
 const absolutePath = __dirname + "/views/index.html";
 const middleWarePath = __dirname + "/public";
 
-// app.get("/", function (req, res) {
+// app.METHOD(PATH, HANDLER_FUNCTION)
+// Get requests to the home page will get a "Hello Express" message printed to the console //
+// app.get("/", (req, res) => {
 //   res.send("Hello Express");
 // });
-app.get('/json', function(req, res) {
+app.get('/json', (req, res) => {
   const mySecret = process.env['MESSAGE_STYLE'];
   if (mySecret === "uppercase") {
     res.json({ "message": "Hello json".toUpperCase() })
@@ -16,6 +18,10 @@ app.get('/json', function(req, res) {
   }
 
 });
+
+// Static is a middleware function that takes 3 arguments - request, response, & next function in cycle //
+// Static is typically used to execute side effects but can end the cycle with a response if conditioins are met //
+app.use("",express.static(req, res, next))
 
 app.use("/public", express.static(middleWarePath));
 app.get("/", (req, res) => res.sendFile(absolutePath));
