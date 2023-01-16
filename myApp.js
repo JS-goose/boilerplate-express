a52e027f2829f55f2c3c581d7fe5b81ac2b10ca2
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2023-01-16 14:06:17 -0600

    built the request logger

--- myApp.js
+++ myApp.js
@@ -4,24 +4,36 @@ let app = express();
 const absolutePath = __dirname + "/views/index.html";
 const middleWarePath = __dirname + "/public";
 
+// Static is a middleware function that takes 3 arguments - request, response, & next function in cycle //
+// Static is typically used to execute side effects but can end the cycle with a response if conditioins are met //
+app.use(
+  // This works and sends info to the command line
+  // "/test",(req,res,next) => {
+  //   console.log(req, "REQUEST");
+  //   console.log(res, "RESPONSE");
+  //   console.log(next, "NEXT");
+  // }
+  (req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+  }
+);
+
+// Basic routing https://expressjs.com/en/starter/basic-routing.html //
+// More details for routing https://expressjs.com/en/guide/routing.html //
 // app.METHOD(PATH, HANDLER_FUNCTION)
 // Get requests to the home page will get a "Hello Express" message printed to the console //
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
 // });
-app.get('/json', (req, res) => {
-  const mySecret = process.env['MESSAGE_STYLE'];
+app.get("/json", (req, res) => {
+  const mySecret = process.env["MESSAGE_STYLE"];
   if (mySecret === "uppercase") {
-    res.json({ "message": "Hello json".toUpperCase() })
+    res.json({ message: "Hello json".toUpperCase() });
   } else {
-    res.json({ "message": "Hello json" })
+    res.json({ message: "Hello json" });
   }
-
 });
-
-// Static is a middleware function that takes 3 arguments - request, response, & next function in cycle //
-// Static is typically used to execute side effects but can end the cycle with a response if conditioins are met //
-app.use("",express.static(req, res, next))
 
 app.use("/public", express.static(middleWarePath));
 app.get("/", (req, res) => res.sendFile(absolutePath));
